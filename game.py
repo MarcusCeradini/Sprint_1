f5ce2a11098937cb6fb09ab6d10f3e9c17fb6731
EvanY-74 <138799210+EvanY-74@users.noreply.github.com>
2026-02-05 10:17:54 -0500

    base pygame code

--- game.py
+++ game.py
@@ -1,2 +1,34 @@
 import pygame
-print(pygame.__version__)
+import sys
+pygame.init()
+ 
+# Colors
+BACKGROUND = (255, 255, 255)
+ 
+# Game Setup
+FPS = 60
+fpsClock = pygame.time.Clock()
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 300
+ 
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('My Game!')
+ 
+def main():
+  looping = True
+  
+  # The main game loop
+  while looping :
+    # Get inputs
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        pygame.quit()
+        sys.exit()
+
+    # Render elements of the game
+    WINDOW.fill(BACKGROUND)
+
+    pygame.display.update()
+    fpsClock.tick(FPS)
+ 
+main()
