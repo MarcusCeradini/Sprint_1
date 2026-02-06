b75a95d6cd4abfb5df34e71915afbb2e02d93f3d
MarcusCeradini <mnceradini@my.waketech.edu>
2026-02-06 08:25:58 -0500

    Worked on generating fish

--- gameState.py
+++ gameState.py
@@ -1,5 +1,10 @@
-from fish import fish, generate_fish
+import random
+from fish import fish, Fish
 
 def start_cast(round):
     generate_fish(round=1)
-    
+
+def generate_fish(current_round):
+    fish_count = current_round * 10
+    for i in range (fish_count):
+        fish.append(Fish())
