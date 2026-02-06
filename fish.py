dce022a99c575fbb757d528d9443c1b9d7aebaba
AyahAbdalla <115049581+AyahAbdalla@users.noreply.github.com>
2026-02-06 08:40:40 -0500

    added basic version of draw function

--- fish.py
+++ fish.py
@@ -2,19 +2,27 @@ import pygame
 import viewport
 
 class Fish:
-    def __init__(self):
-        self.x
-        self.y
-    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        pygame.init()
+        surface = pygame.display.set_mode((400, 300))
+        color = (255, 0, 0)
+
+        pygame.draw.rect(surface, color, pygame.Rect(30, 30, self.x, self.y))
+        pygame.display.flip()
+        pygame.time.wait(6000)
+        pygame.quit()
+    def update(self):
+        pass
+    def move(self):
+        pass
+    def reset(x, y):
+        pass
     def update(self):
         pass
 
-    def draw(self):
-        pygame.Rect(self.x - viewport.x, self.y - viewport.y, 30, 30)
-
-fish = [
-    Fish(),
-    Fish(),
-    Fish(),
-    Fish()
-]
+randomFish = Fish(40,30)
+randomFish.draw()
