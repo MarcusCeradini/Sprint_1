d3bf4e65cd6239004ac93637c716ccc8c0bae122
EvanY-74 <138799210+EvanY-74@users.noreply.github.com>
2026-02-08 15:45:54 -0500

    Added different fish types

--- fish.py
+++ fish.py
@@ -1,19 +1,49 @@
 import pygame
 from viewport import viewport
 
+# abstract class
 class Fish:
-    def __init__(self, x, y):
+    def __init__(self, x, y, xv = 0, yv = 0):
         self.x = x
         self.y = y
+        self.xv = xv
+        self.yv = yv
 
+        self.color = (255, 0, 0)
     def update(self):
         pass
 
     def draw(self, surface):
-        color = (255, 0, 0)
-        pygame.draw.rect(surface, color, pygame.Rect(self.x - viewport.x, self.y - viewport.y, 10, 10))
+        pygame.draw.rect(surface, self.color, pygame.Rect(self.x - viewport.x, self.y - viewport.y, self.width, self.height))
 
     def reset(x, y):
         pass
+
+# Carp, Salmon , Swordfish
+# PufferFish
+
+class Carp(Fish):
+    def __init__(self, x, y, xv = 0, yv = 0):
+        super().__init__(x, y, xv, yv)
+        self.width = 50
+        self.height = 30
+
+        self.color = (200, 0, 0)
+        
+class Salmon(Fish):
+    def __init__(self, x, y, xv = 0, yv = 0):
+        super().__init__(x, y, xv, yv)
+        self.width = 50
+        self.height = 30
+
+        self.color = (255, 0, 0)
+
+class Swordfish(Fish):
+    def __init__(self, x, y, xv = 0, yv = 0):
+        super().__init__(x, y, xv, yv)
+        self.width = 125
+        self.height = 80
+
+        self.color = (0, 0, 220)
 
 fish = []
--- gameState.py
+++ gameState.py
@@ -1,11 +1,26 @@
 import random
 from setup import WINDOW_WIDTH, WINDOW_HEIGHT
-from fish import fish, Fish
+from fish import *
 
 def start_cast(current_round):
     generate_fish(current_round)
 
+PROBABILITIES = {
+    Carp: .6,
+    Salmon: .3,
+    Swordfish: .1
+}
+
+CUMULATIVE_PROBABILITIES = PROBABILITIES.copy()
+running_total = 0
+for key in CUMULATIVE_PROBABILITIES:
+    running_total += PROBABILITIES[key]
+    CUMULATIVE_PROBABILITIES[key] = running_total
+
 def generate_fish(current_round):
     fish_count = current_round * 10
     for i in range(fish_count):
-        fish.append(Fish(random.randint(0, WINDOW_WIDTH), random.randint(0, WINDOW_HEIGHT)))
+        rng = random.random()
+        for FishClass, probability in CUMULATIVE_PROBABILITIES.items():
+            if rng < probability:
+                fish.append(FishClass(random.randint(0, WINDOW_WIDTH), random.randint(0, WINDOW_HEIGHT)))
