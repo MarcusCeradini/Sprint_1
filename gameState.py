5240e011ca9deca5f71672deb3fd6194184d09a8
Bianca <bzbaccay@my.waketech.edu>
2026-02-17 08:04:32 -0500

    Continuing draw_end_screen

--- gameState.py
+++ gameState.py
@@ -34,3 +34,13 @@ def generate_fish(current_round):
              toggles screen for 5 seconds and instructs the user to run the game again to """
         print("You reached the ending of the game")
         pygame.quit()
+
+def draw_end_screen():
+    pass # TODO: Implement end screen
+    image = pygame.image.load("Sprites/LeftSharkFIRE.png")
+    WIDTH, HEIGHT = 800, 600
+
+    image_rect = image.get_rect()
+    image_rect.center = (WIDTH // 2, HEIGHT // 2)
+
+    
